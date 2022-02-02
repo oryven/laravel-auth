b41d3431a9f0de8214e515900a8d19e68d741761
Orazio <orazioventrella@hotmail.it>
2022-02-02 18:38:40 +0100

    feat: login/register

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.main-layout')
+
+@section('content')
+
+@auth
+<h1>{{ Auth::user() -> name }}</h1>
+{{-- <a class="btn btn-secondary" href="{{ route('logout') }}">LOGOUT</a> --}}
+@else
+<h1>If you wanne see my site you have to login/register</h1>
+
+
+<h2>Register</h2>
+<br><hr><br>
+<form action="{{ route('register') }}" method="POST">
+
+    @method('POST')
+    @csrf
+
+    <label for="name">Name</label>
+    <input type="text" name="name"> <br>
+    <label for="email">E-mail</label>
+    <input type="text" name="email"> <br>
+    <label for="password">Password</label>
+    <input type="password" name="password"> <br>
+    <label for="password_confirmation">Password confirm</label>
+    <input type="password" name="password_confirmation"> <br>
+    <br>
+    <input type="submit" value="REGISTER">
+
+</form>
+
+<br><hr><br>
+
+<h2>Login</h2>
+
+<br><hr><br>
+    <form action="{{ route('login') }}" method="POST">
+
+        @method('POST')
+        @csrf
+
+        <label for="email">E-mail</label>
+        <input type="text" name="email"> <br>
+        <label for="password">Password</label>
+        <input type="password" name="password"> <br>
+        <br>
+        <input type="submit" value="LOGIN">
+
+    </form>
+@endauth
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Auth::routes();
+
+// Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'GuestController@home')->name('home');
+Route::post('/login', 'Auth\LoginController@login') -> name('login');
+Route::post('/register', 'Auth\RegisterController@register') -> name('register');
